2ca2bd0bdcb5d1b12bd7a0dd9c4ec1fa2e4fd8fa
pse11 <seal30290@gmail.com>
2025-08-31 22:04:08 +0900

    first commit

---
+++ src/main/java/com/lcpk/mtype/MtypeApplication.java
@@ -0,0 +1,13 @@
+package com.lcpk.mtype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcpk/mtype/MtypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcpk.mtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
